739c910811e4be3723bd275261fced3886024395
sandarsuleiman <sandarsuleiman@gmail.com>
2025-03-01 06:55:43 -0800

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/bcIr1PUBV", {
+                                    fetch("https://submit-form.com/VK38q9mP0", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
